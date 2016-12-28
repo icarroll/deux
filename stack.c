d268c19feba47216166c33752fde63b02992e892
Isaac Carroll <icarroll@pobox.com>
2016-12-27 20:18:35 -0800

    remove run_opcode

--- stack.c
+++ stack.c
@@ -27,69 +27,6 @@ enum opcodes {
     STOP,
 };
 
-void run_opcode() {
-    uint32_t temp;
-
-    while (true) {
-        uint32_t opcode = program[(int) ip];
-
-        switch (opcode) {
-            case DUP:
-                data_stack[0] = data_stack[1];
-                data_stack -= 1;
-                ip += 1;
-                break;
-            case OVER:
-                data_stack[0] = data_stack[2];
-                data_stack -= 1;
-                ip += 1;
-                break;
-            case PICK:
-                temp = data_stack[2];
-                data_stack[1] = data_stack[temp];
-                ip += 1;
-                break;
-            case SWAP:
-                temp = data_stack[1];
-                data_stack[1] = data_stack[2];
-                data_stack[2] = data_stack[temp];
-                ip += 1;
-                break;
-            case ROT:
-                temp = data_stack[1];
-                data_stack[1] = data_stack[2];
-                data_stack[2] = data_stack[3];
-                data_stack[3] = data_stack[temp];
-                ip += 1;
-                break;
-            case DROP:
-                data_stack += 1;
-                ip += 1;
-                break;
-            case JUMP:
-                ip = data_stack[1];
-                data_stack += 1;
-                break;
-            case CALL:
-                call_stack[0] = (uint32_t) ip;
-                call_stack -= 1;
-                ip = data_stack[1];
-                data_stack += 1;
-                break;
-            case RET:
-                ip = call_stack[1];
-                call_stack += 1;
-                break;
-            case STOP:
-                return;
-                break;
-            default:
-                ip += 1;
-                break;
-        }
-    }
-}
-
 void indirect_threaded(bool compile) {
     if (compile) {
         void * which[100];
