3434f7719de3e2395fd1ec6b1e81e4119c9c2fcc
Isaac Carroll <icarroll@pobox.com>
2016-12-27 20:07:13 -0800

    implement indirect_threaded

--- stack.c
+++ stack.c
@@ -1,11 +1,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+enum {STACK_SIZE=1024*1024};
 
 uint32_t * data_stack;
 uint32_t * call_stack;
 uint32_t ip;
 uint32_t * program;
+
+int * source;
+int source_length;
 
 enum opcodes {
     NOOP=0,
@@ -17,7 +23,7 @@ enum opcodes {
     DROP,
     JUMP,
     CALL,
-    RETURN,
+    RET,
     STOP,
 };
 
@@ -29,11 +35,9 @@ void run_opcode() {
 
         switch (opcode) {
             case DUP:
-dup_start:
                 data_stack[0] = data_stack[1];
                 data_stack -= 1;
                 ip += 1;
-dup_end:
                 break;
             case OVER:
                 data_stack[0] = data_stack[2];
@@ -72,7 +76,7 @@ dup_end:
                 ip = data_stack[1];
                 data_stack += 1;
                 break;
-            case RETURN:
+            case RET:
                 ip = call_stack[1];
                 call_stack += 1;
                 break;
@@ -86,5 +90,98 @@ dup_end:
     }
 }
 
+void indirect_threaded(bool compile) {
+    if (compile) {
+        void * which[100];
+
+        which[NOOP] = &&noop;
+        which[DUP] = &&dup;
+        which[OVER] = &&over;
+        which[PICK] = &&pick;
+        which[SWAP] = &&swap;
+        which[ROT] = &&rot;
+        which[DROP] = &&drop;
+        which[JUMP] = &&jump;
+        which[CALL] = &&call;
+        which[RET] = &&ret;
+        which[STOP] = &&stop;
+
+        for (int ix=0 ; ix < source_length ; ix += 1) {
+            program[ix] = (uint32_t) which[source[ix]];
+        }
+
+        return;
+    }
+
+    uint32_t temp;
+
+noop:
+    goto * (ip++);
+noop_end:
+
+dup:
+    data_stack[0] = data_stack[1];
+    data_stack -= 1;
+dup_end:
+    goto * (ip++);
+
+over:
+    data_stack[0] = data_stack[2];
+    data_stack -= 1;
+over_end:
+    goto * (ip++);
+
+pick:
+    temp = data_stack[2];
+    data_stack[1] = data_stack[temp];
+pick_end:
+    goto * (ip++);
+
+swap:
+    temp = data_stack[1];
+    data_stack[1] = data_stack[2];
+    data_stack[2] = data_stack[temp];
+swap_end:
+    goto * (ip++);
+
+rot:
+    temp = data_stack[1];
+    data_stack[1] = data_stack[2];
+    data_stack[2] = data_stack[3];
+    data_stack[3] = data_stack[temp];
+rot_end:
+    goto * (ip++);
+
+drop:
+    data_stack += 1;
+drop_end:
+    goto * (ip++);
+
+jump:
+    ip = data_stack[1];
+    data_stack += 1;
+jump_end:
+
+call:
+    call_stack[0] = (uint32_t) ip;
+    call_stack -= 1;
+    ip = data_stack[1];
+    data_stack += 1;
+call_end:
+
+ret:
+    ip = call_stack[1];
+    call_stack += 1;
+ret_end:
+
+stop:
+    return;
+stop_end:
+
+    0;   // to quiet warning about label at end of block
+}
+
 int main(int argc, char * argv[]) {
+    if (! (data_stack = malloc(STACK_SIZE))) exit(1);
+    if (! (call_stack = malloc(STACK_SIZE))) exit(2);
 }
