37ff94557712817cca79632047105cc6565279f0
Isaac Carroll <icarroll@pobox.com>
2017-04-18 16:38:15 -0700

    remove unused code, implement sym? int?

--- lisp.lua
+++ lisp.lua
@@ -140,205 +140,6 @@ function list(item, ...)
     end
 end
 
-function eval(expr, env)
-    -- io.stdout:write("eval: ") ; show(expr)
-    if type(expr) == "number" then
-        return expr
-    elseif type(expr) ~= "userdata" then
-        error("can't evaluate " .. type(expr))
-    end
-
-    if not getmetatable(expr) then return expr
-    elseif expr.note == "symb" then
-        -- look up variable
-        local temp = lookup(env, expr)
-        if temp then return temp
-        else error("unbound symbol " .. expr:read_string())
-        end
-    elseif expr.note == "cons" then
-        -- evaluate and invoke special form or function
-        local head = expr[0]
-        local tail = expr[1]
-
-        local fn = eval(head, env)
-        return do_invoke(fn, tail, env)
-    else
-        -- other memory blocks self-evaluate
-        return expr
-    end
-end
-
-function eval_list(items, env)
-    if items == raw(0) then return items end
-
-    if getmetatable(items) ~= nil and items.note == "cons" then
-        local head, tail = items[0], items[1]
-        return cons(eval(head, env), eval_list(tail, env))
-    else
-        return eval(items, env)
-    end
-end
-
-function eval_list_one(items, env)
-    if items == raw(0) then return items end
-
-    local head, tail = items[0], items[1]
-    local temp = eval(head, env)
-    if tail == raw(0) then return temp
-    else return eval_list_one(tail, env)
-    end
-end
-
-function lookup(env, key)
-    if env == raw(0) then return nil
-    elseif env[0][0] == key then return env[0][1]
-    else return lookup(env[1], key)
-    end
-end
-
-function extend_env(env, formals, actuals)
-    if formals == raw(0) then return env
-    elseif formals.note == "symb" then
-        return cons(cons(formals, actuals), env)
-    else
-        local fhead, ftail = formals[0], formals[1]
-        local ahead, atail = actuals[0], actuals[1]
-        local tempenv = extend_env(env, fhead, ahead)
-        return extend_env(tempenv, ftail, atail)
-    end
-end
-
-function do_quote(args, env)
-    return args[0]
-end
-
-function do_if(args, env)
-    if args == raw(0) then return args   -- no clauses, return nil
-    elseif args[1] == raw(0) then return eval(args[0], env)   -- else clause
-    else
-        local temp = eval(args[0], env)   -- eval condition
-        if temp == raw(0) then return do_if(args[1][1], env)   -- false
-        else return eval(args[1][0], env)   -- true
-        end
-    end
-end
-
-function do_fn(args, env)
-    local params = args[0]
-    local body = args[1]
-    local newenv = cons(cons(raw(0), raw(0)), env)
-    return list(sym("#<fn>"), newenv, params, body)
-end
-
-function do_mac(args, env)
-    local params = args[0]
-    local body = args[1]
-    local newenv = cons(cons(raw(0), raw(0)), env)
-    return list(sym("#<mac>"), newenv, params, body)
-end
-
-function do_invoke(fn, actuals, env)
-    if fn[0] == sym("#<fn>") then
-        local fnenv, formals, body = fn[1][0], fn[1][1][0], fn[1][1][1][0]
-        actuals = eval_list(actuals, env)
-        newenv = extend_env(fnenv, formals, actuals)
-        return eval_list_one(body, newenv)
-    elseif fn[0] == sym("#<mac>") then
-        local fnenv, formals, body = fn[1][0], fn[1][1][0], fn[1][1][1][0]
-        newenv = extend_env(fnenv, formals, actuals)
-        local command = eval_list_one(body, newenv)
-        return eval(command, env)
-    elseif fn[0] == sym("#<special>") then
-        local which = fn[1][0]
-        if which == sym("quote") then return do_quote(actuals, env)
-        elseif which == sym("if") then return do_if(actuals, env)
-        elseif which == sym("fn") then return do_fn(actuals, env)
-        elseif which == sym("mac") then return do_mac(actuals, env)
-        elseif which == sym("new") then return do_new(actuals, env)
-        elseif which == sym("set") then return do_set(actuals, env)
-        elseif which == sym("builtin") then return do_builtin(actuals, env)
-        end
-    elseif fn[0] == sym("#<builtin>") then
-        local which = fn[1][0]
-        actuals = eval_list(actuals, env)
-        if which == sym("issym") then
-            local item = actuals[0]
-            if type(item) == "userdata" and getmetatable(item) ~= nil
-                and item.note == "symb" then return sym("t")
-            else return raw(0)
-            end
-        elseif which == sym("iscons") then
-            local item = actuals[0]
-            if type(item) == "userdata" and getmetatable(item) ~= nil
-                and item.note == "cons" then return sym("t")
-            else return raw(0)
-            end
-        elseif which == sym("issame") then
-            local item1, item2 = actuals[0], actuals[1][0]
-            if item1 == item2 then return sym("t")
-            else return raw(0)
-            end
-        elseif which == sym("plus") then
-            return do_plus(actuals)
-        elseif which == sym("minus") then
-            return do_minus(actuals)
-        elseif which == sym("times") then
-            return do_times(actuals)
-        elseif which == sym("read_sexpr") then
-            local line = readline("")
-            return parse(line)
-        elseif which == sym("write_sexpr") then
-            show(actuals[0])
-            return raw(0)
-        end
-    else
-        error("bad invoke")
-    end
-end
-
-function do_plus(items)
-    if items == raw(0) then return 0
-    else return items[0] + do_plus(items[1])
-    end
-end
-
-function do_minus(items)
-    if items == raw(0) then return 0
-    else return items[0] - do_minus(items[1])
-    end
-end
-
-function do_times(items)
-    if items == raw(0) then return 1
-    else return items[0] * do_times(items[1])
-    end
-end
-
-function do_new(args, env)
-    local key, value = args[0], eval(args[1][0], env)
-    env[1] = cons(cons(key, value), env[1])
-    return value
-end
-
-function do_set(args, env)
-    local key, value = args[0], eval(args[1][0], env)
-
-    local function lookupset(env)
-        if env == raw(0) then error("unbound variable " .. key:read_string())
-        elseif env[0][0] == key then env[0][1] = value
-        else lookupset(env[1])
-        end
-    end
-
-    lookupset(env)
-    return value
-end
-
-function do_builtin(args, env)
-    local which = args[0]
-    return list(sym("#<builtin>"), which)
-end
-
 function show(item)
     seen = {}
 
@@ -376,81 +177,6 @@ function show(item)
     show_one(item)
     io.stdout:write("\n")
 end
-
-lisp = {}
-
-function lisp.repl(env)
-    local stdenv
-    if not env then
-        env = root[ENVIRONMENT]
-        stdenv = true
-    else stdenv = false
-    end
-
-    while true do
-        ::start::
-
-        local line = readline("lisp> ")
-        if not line then print() ; break end
-        local expr
-        ok, msg = xpcall(function () expr = parse(line) end, debug.traceback)
-        if not ok then
-            print("parse error: " .. msg)
-            goto start
-        end
-
-        if not expr then goto start end
-
-        local result
-        ok, msg = xpcall(function () result = eval(expr, env) end, debug.traceback)
-        if not ok then
-            print("eval error: " .. msg)
-            goto start
-        end
-        if result ~= raw(0) then show(result) end
-    end
-
-    if stdenv then root[ENVIRONMENT] = env
-    else return env
-    end
-end
-
-function lisp.load(filename, env)
-    if not env then env = root[ENVIRONMENT] end
-
-    local f = io.open(filename, "r")
-    local text = f:read("a")
-    f:close()
-
-    text = skip_space(text)
-    while #text > 0 do
-        local temp = parse_cons_item(text)
-        if not temp.result then
-            error(string.format("unexpected %s", text:sub(1,1)))
-        end
-
-        eval(temp.result, env)
-        text = skip_space(temp.rest)
-    end
-end
-
---[[
--- Populate environment and run interpreter
-if root[ENVIRONMENT] == raw(0) then
-    root[ENVIRONMENT] = list(
-        cons(raw(0), raw(0)),
-        cons(sym("quote"), list(sym("#<special>"), sym("quote"))),
-        cons(sym("if"), list(sym("#<special>"), sym("if"))),
-        cons(sym("fn"), list(sym("#<special>"), sym("fn"))),
-        cons(sym("mac"), list(sym("#<special>"), sym("mac"))),
-        cons(sym("new"), list(sym("#<special>"), sym("new"))),
-        cons(sym("set"), list(sym("#<special>"), sym("set"))),
-        cons(sym("builtin"), list(sym("#<special>"), sym("builtin"))))
-end
-
-lisp.load("lisp.lisp")
-lisp.repl()
-]]
 
 TAG_MASK = 3
 PTR_TAG = 0
@@ -674,7 +400,7 @@ function emit_code_for(cw, expr, symtab)
             local ix = cw:desc_value(expr[1][0])
             cw:emit(READ_FAR(cw:push(), DESCRIPTOR, ix))
         elseif expr[0] == sym("cons") then
-            emit_code_for_cons(cw)
+            cw:emit(ALLOCATE_CONS(cw:push()))
             local cons_ix = cw:top()
             emit_code_for(cw, expr[1][0], symtab)
             cw:emit(WRITE_FAR(cons_ix, 0, cw:pop()))
@@ -734,6 +460,16 @@ function emit_code_for(cw, expr, symtab)
             cw:emit(AND_imm8_raw(cw:top(), cw:top(), TAG_MASK))
             cw:emit(SUB_imm8_raw(cw:top(), cw:top(), CONS_TAG))
             cw:emit(IS_ZERO_raw(cw:top(), cw:top()))
+        elseif expr[0] == sym("sym?") then
+            emit_code_for(cw, expr[1][0], symtab)
+            cw:emit(AND_imm8_raw(cw:top(), cw:top(), TAG_MASK))
+            cw:emit(SUB_imm8_raw(cw:top(), cw:top(), SYM_TAG))
+            cw:emit(IS_ZERO_raw(cw:top(), cw:top()))
+        elseif expr[0] == sym("int?") then
+            emit_code_for(cw, expr[1][0], symtab)
+            cw:emit(AND_imm8_raw(cw:top(), cw:top(), TAG_MASK))
+            cw:emit(SUB_imm8_raw(cw:top(), cw:top(), INT_TAG))
+            cw:emit(IS_ZERO_raw(cw:top(), cw:top()))
         elseif expr[0] == sym("same?") then
             emit_code_for(cw, expr[1][0], symtab)
             emit_code_for(cw, expr[1][1][0], symtab)
@@ -744,7 +480,7 @@ function emit_code_for(cw, expr, symtab)
         elseif expr[0] == sym("list") then
             local items = expr[1]
             if getmetatable(items) and items.note == "cons" then
-                emit_code_for_cons(cw)   -- Emit head cons of list
+                cw:emit(ALLOCATE_CONS(cw:push()))   -- Emit head cons of list
                 local cons_ix = cw:top()
                 cw:emit(COPY(cw:push(), cons_ix))   -- Copy for building list
                 emit_code_for_create_list(cw, items, symtab)
@@ -796,7 +532,7 @@ function emit_code_for(cw, expr, symtab)
                 emit_code_for(cw, expr[0], symtab)
                 local actuals = expr[1]
                 if getmetatable(actuals) and actuals.note == "cons" then
-                    emit_code_for_cons(cw)   -- Emit head cons of list
+                    cw:emit(ALLOCATE_CONS(cw:push()))   -- Emit head cons of list
                     local cons_ix = cw:top()
                     cw:emit(COPY(cw:push(), cons_ix))   -- Copy for building list
                     emit_code_for_create_list(cw, actuals, symtab)
@@ -866,7 +602,7 @@ function emit_code_for_create_list(cw, actuals, symtab)
     cw:emit(WRITE_FAR(cons_ix, 0, cw:pop()))
 
     if getmetatable(actuals[1]) and actuals[1].note == "cons" then
-        emit_code_for_cons(cw)
+        cw:emit(ALLOCATE_CONS(cw:push()))
         cw:emit(WRITE_FAR(cons_ix, 1, cw:top()))
         cw:emit(COPY(cons_ix, cw:pop()))
         emit_code_for_create_list(cw, actuals[1], symtab)
@@ -875,19 +611,6 @@ function emit_code_for_create_list(cw, actuals, symtab)
         emit_code_for(cw, actuals[1], symtab)
         cw:emit(WRITE_FAR(cons_ix, 1, cw:pop()))
     end
-end
-
-function emit_code_for_cons(cw)
-    --[[
-    cw:emit(ALLOCATE_ALLPTR_imm16(cw:push(), 2))
-    local cons_ix = cw:top()
-    cw:emit(OR_imm8_raw(cons_ix, cons_ix, CONS_TAG))
-    local note = make_note("cons")
-    cw:emit(SET_16l_raw(cw:push(),  low16(note)))
-    cw:emit(SET_16h_raw(cw:top(),  high16(note)))
-    cw:emit(SET_NOTE(cons_ix, cw:pop()))
-    ]]
-    cw:emit(ALLOCATE_CONS(cw:push()))
 end
 
 function call_vm(sub_arec, arg)
